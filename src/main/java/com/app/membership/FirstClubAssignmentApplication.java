fd14fb1720b1cf2c391a073e2d7bf74a1164d148
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2026-05-17 00:06:10 +0530

    init

---
+++ src/main/java/com/app/membership/FirstClubAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.app.membership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstClubAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstClubAssignmentApplication.class, args);
+    }
+
+}
